7ab5f6b8e1427496926578bf91cc990022f57d92
tapo44ek <136156340+tapo44ek@users.noreply.github.com>
2024-05-21 10:01:00 +0300

    Add files via upload

---
+++ sogl_html.py
@@ -0,0 +1,89 @@
+html1 = '<!DOCTYPE html>' \
+        '<html style="font-size: 16px;" lang="ru"><head>' \
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0">' \
+        '<meta charset="utf-8">' \
+        '<meta name="keywords" content="The CREATES framework">' \
+        '<meta name="description" content="">' \
+        '<title>DOCUMENTS</title>' \
+        '<link rel="stylesheet" href="nicepage.css" media="screen">' \
+        '<link rel="stylesheet" href="Page-8.css" media="screen">' \
+        '<script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>' \
+        '<script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>' \
+        '<meta name="generator" content="Nicepage 6.4.4, nicepage.com">' \
+        '<link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">' \
+        '<script type="application/ld+json">{' \
+        '"@context": "http://schema.org",' \
+        '"@type": "Organization",' \
+        '"name": "",' \
+        '"url": "/"' \
+        '}</script>' \
+        '<meta name="theme-color" content="#4470f4">' \
+        '<meta property="og:title" content="Page 8">' \
+        '<meta property="og:type" content="website">' \
+        '<link rel="canonical" href="/">' \
+        '<meta data-intl-tel-input-cdn-path="intlTelInput/"></head>' \
+        '<body data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="ru">' \
+        '<section class="u-clearfix u-palette-5-dark-2 u-section-1" id="carousel_1d1f">' \
+        '<div class="u-clearfix u-sheet u-sheet-1">' \
+        '<div class="custom-expanded data-layout-selected u-clearfix u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xl u-layout-wrap u-layout-wrap-1">' \
+        '<div class="u-gutter-0 u-layout">' \
+        '<div class="u-layout-row">' \
+        '<div class="u-size-13">' \
+        '<div class="u-layout-col">' \
+        '<div class="u-container-style u-hidden-sm u-hidden-xs u-image u-layout-cell u-left-cell u-size-60 u-image-1" data-image-width="1920" data-image-height="960">' \
+        '<div class="u-container-layout u-container-layout-1"></div>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '<div class="u-size-47">' \
+        '<div class="u-layout-col">' \
+        '<div class="u-align-left u-border-no-bottom u-border-no-left u-border-no-right u-border-no-top u-container-style u-layout-cell u-right-cell u-size-60 u-white u-layout-cell-2">' \
+        '<div class="u-container-layout u-container-layout-2">' \
+        '<h2 class="u-align-left u-text u-text-default u-text-1">' \
+        '<span style="font-weight: 700;">Соглы</span>' \
+        '<br>' \
+        '<span style="font-size: 2.5rem;">на рассмотрении</span>' \
+        '</h2>' \
+        '<img class="u-image u-image-default u-image-2" src="images/56d1793a-dd6e-d994-bdfb-c10993b5d9a3.png" alt="" data-image-width="429" data-image-height="515">' \
+        '<p class="u-align-left u-text u-text-default u-text-2">Отсортированы по дате создания согла<br>от новых к старым ' \
+        '</p>' \
+        '<div class="u-align-center-xs u-expanded-width u-table u-table-responsive u-table-1">' \
+        '<table class="u-table-entity">' \
+        '<colgroup>' \
+        '<col width="8.7%">' \
+        '<col width="22.6%">' \
+        '<col width="20.1%">' \
+        '<col width="48.6%">' \
+        '</colgroup>' \
+        '<thead class="u-table-header u-table-header-1">' \
+        '<tr style="height: 50px;">' \
+        '<th class="u-border-2 u-border-grey-75 u-border-no-left u-border-no-right u-table-cell">№<br>П/п ' \
+        '</th>' \
+        '<th class="u-border-2 u-border-grey-75 u-border-no-left u-border-no-right u-table-cell">№<br>согла' \
+        '</th>' \
+        '<th class="u-border-2 u-border-grey-75 u-border-no-left u-border-no-right u-table-cell">дата<br>согла' \
+        '</th>' \
+        '<th class="u-border-2 u-border-grey-75 u-border-no-left u-border-no-right u-table-cell">краткое<br>содержание' \
+        '</th>' \
+        '</tr>' \
+        '</thead>' \
+        '<tbody class="u-table-body u-table-body-1">'
+
+
+html2 = '</tbody>' \
+        '</table>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '</div>' \
+        '</section>' \
+        '</body></html>'
+
+server_ip = '77.222.40.238'
+server_user = 'viktooooor'
+server_pass = 'ctkghaf3nNsG686-'
